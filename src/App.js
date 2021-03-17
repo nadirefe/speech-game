35c9522cf4e8c688a0d657f7ba3d7f154c1f3113
Efe <efenadir@sabanciuniv.edu>
2021-03-17 12:40:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+// const recognition = new SpeechRecognition();
+// window.SpeechRecognition =
+//   window.webkitSpeechRecognition || window.SpeechRecognition;
+
+// const recognition = new SpeechRecognition();
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = "en-US";
+
+const App = () => {
+  const [isListening, setIsListening] = useState(false);
+  const [speech, setSpeech] = useState(null);
+  // const [savedSpeech, setSavedSpeech] = useState([]);
+
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue..");
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log("Stopped Mic on Click");
+        console.log(speech);
+      };
+    }
+    mic.onstart = () => {
+      console.log("Mics on");
+    };
+    mic.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      setSpeech(transcript);
+
+      mic.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
+      <button onClick={() => setIsListening(!isListening)}>Start/Stop</button>
     </div>
   );
-}
+};
 
 export default App;
